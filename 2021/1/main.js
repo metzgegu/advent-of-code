2d507d426d5e29ed166de2b4617fa8ae308cff87
metzgegu <guillaume.metzger@wizbii.com>
2021-12-02 15:14:07 +0100

    Fix lint

--- 2021/1/main.js
+++ 2021/1/main.js
@@ -1,29 +1,29 @@
 const fs = require('fs')
 
 const getResultPartOne = (tab) => {
-    return tab.reduce((acc, value) => ({
-        prev: value,
-        nbIncr: (acc.prev && value > acc.prev) ? acc.nbIncr + 1 : acc.nbIncr
-    }), {nbIncr: 0}).nbIncr
+  return tab.reduce((acc, value) => ({
+    prev: value,
+    nbIncr: (acc.prev && value > acc.prev) ? acc.nbIncr + 1 : acc.nbIncr
+  }), { nbIncr: 0 }).nbIncr
 }
 
 const getResultPartTwo = (tab) => {
-    return tab.map((value, index) => !!tab[index + 1] && !!tab[index + 2] ? value + tab[index + 1] + tab[index + 2] : -1)
-        .filter(value => value !== -1)
-        .reduce((acc, value) => (
-            {prev: value, nbIncr: (acc.prev && parseInt(value) > parseInt(acc.prev)) ? acc.nbIncr + 1 : acc.nbIncr}
-    ), {nbIncr: 0}).nbIncr
+  return tab.map((value, index) => !!tab[index + 1] && !!tab[index + 2] ? value + tab[index + 1] + tab[index + 2] : -1)
+    .filter(value => value !== -1)
+    .reduce((acc, value) => (
+      { prev: value, nbIncr: (acc.prev && parseInt(value) > parseInt(acc.prev)) ? acc.nbIncr + 1 : acc.nbIncr }
+    ), { nbIncr: 0 }).nbIncr
 }
 
 fs.readFile('input.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
+  if (err) {
+    console.error(err)
+    return
+  }
 
-    const tab = data.split('\n').map(value => parseInt(value))
+  const tab = data.split('\n').map(value => parseInt(value))
 
-    console.log('Result part one :', getResultPartOne(tab))
+  console.log('Result part one :', getResultPartOne(tab))
 
-    console.log('Result part two :', getResultPartTwo(tab))
+  console.log('Result part two :', getResultPartTwo(tab))
 })
--- 2021/2/main.js
+++ 2021/2/main.js
@@ -1,42 +1,42 @@
 const fs = require('fs')
 
 const getResultPartOne = (tab) => {
-    const resultObject = tab.reduce((acc, value) => {
-        const t = value.split(" ")
-        return {
-            ...acc,
-            ...(t[0] === 'forward' && { h: acc.h + parseInt(t[1]) }),
-            ...(t[0] === 'up' && { d: acc.d - parseInt(t[1]) }),
-            ...(t[0] === 'down' && { d: acc.d + parseInt(t[1]) })
-        }
-    }, { h: 0, d: 0 })
+  const resultObject = tab.reduce((acc, value) => {
+    const t = value.split(' ')
+    return {
+      ...acc,
+      ...(t[0] === 'forward' && { h: acc.h + parseInt(t[1]) }),
+      ...(t[0] === 'up' && { d: acc.d - parseInt(t[1]) }),
+      ...(t[0] === 'down' && { d: acc.d + parseInt(t[1]) })
+    }
+  }, { h: 0, d: 0 })
 
-    return resultObject.h * resultObject.d
+  return resultObject.h * resultObject.d
 }
 
 const getResultPartTwo = (tab) => {
-    const resultObject = tab.reduce((acc, value) => {
-        const t = value.split(" ")
-        return {
-            ...acc,
-            ...(t[0] === 'forward' && { d: acc.d + parseInt(t[1]) * acc.aim, h: acc.h + parseInt(t[1]) }),
-            ...(t[0] === 'up' && { aim: acc.aim - parseInt(t[1]) }),
-            ...(t[0] === 'down' && { aim: acc.aim + parseInt(t[1]) })
-        }
-    }, { h: 0, d: 0, aim: 0 })
+  const resultObject = tab.reduce((acc, value) => {
+    const t = value.split(' ')
+    return {
+      ...acc,
+      ...(t[0] === 'forward' && { d: acc.d + parseInt(t[1]) * acc.aim, h: acc.h + parseInt(t[1]) }),
+      ...(t[0] === 'up' && { aim: acc.aim - parseInt(t[1]) }),
+      ...(t[0] === 'down' && { aim: acc.aim + parseInt(t[1]) })
+    }
+  }, { h: 0, d: 0, aim: 0 })
 
-    return resultObject.h * resultObject.d
+  return resultObject.h * resultObject.d
 }
 
 fs.readFile('input.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
+  if (err) {
+    console.error(err)
+    return
+  }
 
-    const tab = data.split('\n')
+  const tab = data.split('\n')
 
-    console.log('Result part one :', getResultPartOne(tab))
+  console.log('Result part one :', getResultPartOne(tab))
 
-    console.log('Result part two :', getResultPartTwo(tab))
+  console.log('Result part two :', getResultPartTwo(tab))
 })
